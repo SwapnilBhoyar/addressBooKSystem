659046ea92791b1d2b9c7ac852a992820a11da15
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-05-02 01:41:41 +0530

    Added contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,5 +1,39 @@
-class AddressBookMain{
-    public static void main(String args[]) {
-    System.out.println("Wellcome to Address Book Program!");
+import java.util.Scanner;
+class contactDetail {
+    public final String firstname, lastname;
+    public final String address, city, state;
+    public final String zip;
+    public final String phoneNumber;
+    public final String email;
+
+    public contactDetail(String firstname, String lastname, String address, String city, String state, String zip, String phonenumber, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phonenumber;
+        this.email = email;
+    }
+    public void printContactDetails() {
+    System.out.println("Firstname : " + firstname);
+    System.out.println("Lastname : " + lastname);
+    System.out.println("Address : " + address);
+    System.out.println("City : " + city);
+    System.out.println("State : " + state);
+    System.out.println("Zip : " + zip);
+    System.out.println("PhoneNumber : " + phoneNumber);
+    System.out.println("Email : " + email);
     }
 }
+
+public class AddressBookMain {
+    public static void main(String args[]) {
+        System.out.println("Wellcome to Address Book Program!");
+
+        contactDetail contact = new contactDetail("Swapnil", "Bhoyar", "Camp", "Pune", "Maharashtra", "411001", "9876543210", "swapnilbhoyar@gmail.com");
+        contact.printContactDetails();
+        }
+}
+
