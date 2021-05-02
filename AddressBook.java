4a04bab41935cf0a5af512aea4e1d3c95a7baf9f
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-05-03 02:26:32 +0530

    Refactored

---
+++ AddressBook.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class addressBook {
+    private ArrayList<contactDetail> contactList = new ArrayList<contactDetail>();
+    Scanner sc = new Scanner(System.in);
+        
+	public void insertContact(contactDetail contactDetailObject) {
+		contactList.add(contactDetailObject);
+	}
+
+	public void editContact(String nameToEdit) {
+        for(contactDetail editContact: contactList) {
+			if(editContact.getFirstName().equals(nameToEdit)) {
+	        	
+				System.out.println("Edit Firstname : ");
+	        	String firstname = sc.nextLine();
+	        	editContact.setFirstName(firstname);
+	        	
+				System.out.println("Edit Lastname : ");
+	        	String lastname = sc.nextLine();
+				editContact.setLastName(lastname);
+                
+				System.out.println("Edit Address : ");
+                String address = sc.nextLine();
+				editContact.setAddress(address);
+                
+				System.out.println("Edit City : ");
+                String city = sc.nextLine();
+				editContact.setCity(city);
+                
+				System.out.println("Edit State : ");
+                String state = sc.nextLine();
+				editContact.setState(state);
+                
+				System.out.println("Edit Zip : ");
+                int zip = sc.nextInt();
+				editContact.setZip(zip);
+                
+				System.out.println("Edit PhoneNumber : ");
+                long phonenumber = sc.nextLong();
+				scan.nextLine();
+				editContact.setPhoneNo(phonenumber);
+                
+				System.out.println("Edit Email : ");
+                String email = sc.nextLine();
+				editContact.setEmail(email);
+            }
+        }
+    }
+
+    public void printContactDetails() {
+        for(contactDetail getInfo: contactList) {
+            System.out.println("Firstname : " + getInfo.getFirstName());
+            System.out.println("Lastname : " + getInfo.getLastName());
+            System.out.println("Address : " + getInfo.getAddress());
+            System.out.println("City : " + getInfo.getCity());
+            System.out.println("State : " + getInfo.getState());
+            System.out.println("Zip : " + getInfo.getZip());
+            System.out.println("PhoneNumber : " + getInfo.getPhoneNo());
+            System.out.println("Email : " + getInfo.getEmail());
+        }
+    }
+
+}
+
